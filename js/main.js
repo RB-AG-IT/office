9f95296d1ad56bbc75d38a6445af7b78d3e43c72
RB-AG-IT <verwaltung@rb-inside.de>
2026-01-30 17:53:33 +0100

    Rename 'E-Mail senden' zu 'Willkommensmail senden' in Neumitglieder-Dropdown

--- js/main.js
+++ js/main.js
@@ -7238,7 +7238,7 @@ function renderRecordsTable() {
                             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                                 <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
                             </svg>
-                            E-Mail senden
+                            Willkommensmail senden
                         </div>
                         <div class="dropdown-item" onclick="handleDropdownAction('pdf', this.parentElement)">
                             <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
